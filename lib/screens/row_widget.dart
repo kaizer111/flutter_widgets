d98f673e276a37c4e831c934184183ba97e645c8
vishal <mahatovishal2001@email.com>
2022-06-11 10:38:29 +0530

    first commit

---
+++ lib/screens/row_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class RowWidget extends StatelessWidget {
+  const RowWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Exploring row widget"),backgroundColor: Colors.deepPurple),
+    );
+  }
+}
