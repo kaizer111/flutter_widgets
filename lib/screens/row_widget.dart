61dcf48f9525ac70be0ff4256fc29c92d8c16810
vishal <mahatovishal2001@email.com>
2022-06-16 11:28:56 +0530

    AppBar

--- lib/screens/row_widget.dart
+++ lib/screens/row_widget.dart
@@ -1,22 +1,41 @@
 import 'package:flutter/material.dart';
+
 class RowWidget extends StatelessWidget {
   const RowWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(centerTitle:true ,title:Text("Exploring row widget"),backgroundColor: Colors.deepPurpleAccent),
-      body: Row(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-        children: [
-
-          Text('naruto',style: TextStyle(fontSize: 20),),
-          Container(height:  100,width: 50,color: Colors.amber,),
-          Text("Minato",style: TextStyle(fontSize: 20),),
-        ],
-      ),
+      appBar: AppBar(
+          //NOTE :  Title parameter can take any widget
+          centerTitle: true,
+          shadowColor: Colors.deepPurple,
+          iconTheme: IconThemeData(color: Colors.white),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                print('icon tapped');
+                print('icon tapped twice');
+              },
+            )
+          ],
+          leading: Icon(
+            Icons.arrow_back,
+          ),
+          title: Text('KAKASHI'),
+          backgroundColor: Colors.deepPurpleAccent),
+      // body: Row(
+      //   crossAxisAlignment: CrossAxisAlignment.end,
+      //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //
+      //   children: [
+      //
+      //     Text('naruto',style: TextStyle(fontSize: 20),),
+      //     Container(height:  100,width: 50,color: Colors.amber,),
+      //     Text("Minato",style: TextStyle(fontSize: 20),),
+      //   ],
+      // ),
     );
   }
 }
