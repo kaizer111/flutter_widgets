ac37da432a2cd2988192ce7c75f49d6f763362ed
vishal <mahatovishal2001@email.com>
2022-06-18 10:06:08 +0530

    Bottom navugation bar

--- lib/screens/row_widget.dart
+++ lib/screens/row_widget.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
 
-class RowWidget extends StatelessWidget {
-  const RowWidget({Key? key}) : super(key: key);
+class RowWidget extends StatefulWidget {
+
+  @override
+  State<RowWidget> createState() => _RowWidgetState();
+}
+
+class _RowWidgetState extends State<RowWidget> {
+  int selectedindex=0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedindex,
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.deepPurpleAccent,
+          backgroundColor: Colors.white70,
+        onTap: (index) {
+          //NOTE : whenever we call setstate in a statefull widgetit rebuilds the whole UI
+          setState((){
+            selectedindex=index;
+          });
+        },
+
+        items: [BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home',),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart),label: 'cart',),
+          BottomNavigationBarItem(icon: Icon(Icons.person) , label: 'account',),
+        ],
+
+      ),
       appBar: AppBar(
           //NOTE :  Title parameter can take any widget
           centerTitle: true,
           shadowColor: Colors.deepPurple,
           iconTheme: IconThemeData(color: Colors.white),
+
           actions: [
             IconButton(
               icon: Icon(Icons.search),
