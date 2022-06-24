87efdabdb866d7c8b4e98c947d16439eb057afcb
vishal <mahatovishal2001@email.com>
2022-06-24 08:58:20 +0530

    Textfieldform number pad

--- lib/screens/textfieldscreen.dart
+++ lib/screens/textfieldscreen.dart
@@ -15,13 +15,15 @@ class TextfieldScreen extends StatelessWidget {
         child: SizedBox(
           width: 300,
           child: TextFormField(
+            keyboardType: TextInputType.number,
             decoration: InputDecoration(
-                labelText: 'name',
+                labelText: 'phone',
               labelStyle: TextStyle(fontSize: 25,color: Colors.greenAccent,),
               // enabledBorder: OutlineInputBorder(
               //   borderSide: BorderSide(color: Colors.green,style: BorderStyle.solid,width: 1.5,),
               //   borderRadius: BorderRadius.all(Radius.circular(10)),
               // ),
+
                 focusedBorder: OutlineInputBorder(
                   borderSide: BorderSide(color: Colors.green,style: BorderStyle.solid,width: 1.5,),
                   borderRadius: BorderRadius.all(Radius.circular(10)),
